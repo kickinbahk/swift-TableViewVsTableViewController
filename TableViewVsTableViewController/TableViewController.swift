35659d0e3684ba99b039944c3a4c7e4841ed958d
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-06 19:48:35 -0700

    add table view and table view controller

---
+++ TableViewVsTableViewController/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  TableViewVsTableViewController
+//
+//  Created by Josiah Mory on 7/6/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- TableViewVsTableViewController/ViewController.swift
+++ TableViewVsTableViewController/ViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
   }
 
 
