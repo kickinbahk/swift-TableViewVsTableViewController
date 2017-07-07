a48273d1b748bac12d58ff236e701a27eae97a0f
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-06 20:01:24 -0700

    show static cells in tableview controller

--- TableViewVsTableViewController/TableViewController.swift
+++ TableViewVsTableViewController/TableViewController.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "This is a UI Table View Controller"
+    return cell
+  }
 }
